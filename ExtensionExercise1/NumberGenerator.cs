8cf897a58940aa621c9c33d8326b22a9b485db17
Nicolas Espitalier <nespitalier013@gmail.com>
2020-10-17 23:16:16 -0400

    Creation of the number generator

--- ExtensionExercise1/NumberGenerator.cs
+++ ExtensionExercise1/NumberGenerator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtensionExercise1
+namespace EnvironmentGoLex
 {
     static class NumberGenerator
     {
--- ExtensionExercise1/Program.cs
+++ ExtensionExercise1/Program.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtensionExercise1
+namespace EnvironmentGoLex
 {
     static class Program
     {
